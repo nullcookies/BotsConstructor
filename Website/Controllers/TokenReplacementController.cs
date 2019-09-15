cc5fa85fe3c3c6f80dd066380f7f6bfc908f6d73
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-09-15 16:58:00 +0300

    страницу для смены нового токена нужно облагородить

--- Website/Controllers/TokenReplacementController.cs
+++ Website/Controllers/TokenReplacementController.cs
@@ -1,32 +1,17 @@
-﻿using System;
-using Newtonsoft.Json;
-using System.Linq;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Website.Models;
-using System.Collections.Generic;
-using Website.Other;
-
-using System.Net;
-using System.IO;
-using Newtonsoft.Json.Linq;
-using Microsoft.AspNetCore.Hosting;
-using Website.Other.Filters;
-using System.Net.WebSockets;
-using System.Threading.Tasks;
-using System.Text;
-using System.Threading;
-using Website.Services;
+﻿using DataLayer.Models;
 using DataLayer.Services;
-using DataLayer.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using Telegram.Bot;
+using Website.Other;
+using Website.Other.Filters;
+
+
 namespace Website.Controllers
 {
     public class TokenReplacementController : Controller
     {
-
-
         ApplicationContext context;
         IHostingEnvironment _appEnvironment;
         StupidLogger _logger;
@@ -56,37 +41,25 @@ namespace Website.Controllers
         [TypeFilter(typeof(CheckAccessToTheBot))]
         public IActionResult TokenChange(int botId, string token)
         {
-            if (token?.Length == 45)
+            
+            var bot = context.Bots.Find(botId);
+            try
             {
-                var bot = context.Bots.Find(botId);
-                try
-                {
-                    bot.BotName = new TelegramBotClient(token).GetMeAsync().Result.Username;                
-                    bot.Token = token;
-                    context.SaveChanges();
-                    ViewData["currentToken"] = token;
-                }catch(Exception ee)
-                {
-                    int? accountId = Stub.GetAccountIdFromCookies(HttpContext);
-                    _logger.Log(LogLevelMyDich.USER_ERROR, Source.WEBSITE, $"Сайт. Установка токена для бота " +
-                        $"botId={botId}, accountId= {accountId}, token = {token}. Не удалось " +
-                        $"узнать username бота.  Возможно, такой токен не существует. exception="+
-                       ee.Message);
-
-                    ModelState.AddModelError("", "Ошибка обработки токена.");
-                    string currentToken = context.Bots.Find(botId).Token;
-                    ViewData["currentToken"] = currentToken;
-                }
-            }
-            else
+                bot.BotName = new TelegramBotClient(token).GetMeAsync().Result.Username;                
+                bot.Token = token;
+                context.SaveChanges();
+            }catch(Exception ee)
             {
-                ModelState.AddModelError("", "Неверная длина токена");
-                _logger.Log(LogLevelMyDich.USER_ERROR, Source.WEBSITE, $"Введён токен неверной длины token={token}");
-                string currentToken = context.Bots.Find(botId).Token;
-                ViewData["currentToken"] = currentToken;
+                int? accountId = Stub.GetAccountIdFromCookies(HttpContext);
+
+                _logger.Log(LogLevelMyDich.USER_ERROR, Source.WEBSITE, $"Сайт. Установка токена для бота " +
+                    $"botId={botId}, accountId= {accountId}, token = {token}. Не удалось " +
+                    $"узнать username бота.  Возможно, такой токен не существует. ", ex: ee);
+
+                ModelState.AddModelError("", "Ошибка обработки токена.");
             }
-
-
+          
+            ViewData["currentToken"] = bot.Token;
             ViewData["botId"] = botId;
 
             return View();
