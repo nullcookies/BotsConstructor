c5e637781ade6627e364e3ae158dd7fb35f07758
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-09-17 22:02:24 +0300

    Добвил вкладку тест. Если в конфиге не "Development" то пунтк в меню не появится и контроллер вернёт 404

--- Website/Controllers/Test1Controller.cs
+++ Website/Controllers/Test1Controller.cs
@@ -1,5 +1,6 @@
 ﻿using DataLayer.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Razor.Language.Extensions;
 
@@ -7,10 +8,21 @@ namespace Website.Controllers
 {
     public class Test1Controller : Controller
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public Test1Controller(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
-            //int accountId = (int) HttpContext.Items["acoountId"];
+            if (!_hostingEnvironment.IsDevelopment())
+            {
+                return StatusCode(404);
+            }
+
             return View();
         }
         
