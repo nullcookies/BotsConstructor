4824244dc3dca4b5ec6a92d3c5a349c09f74b12a
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-09-19 21:41:31 +0300

    mf

--- Website/Program.cs
+++ Website/Program.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace Website
@@ -7,7 +10,21 @@ namespace Website
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            try
+            {
+                CreateWebHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                string writePath = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
+                writePath = Path.Combine(writePath, "mydich.txt");
+
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(e.Message);
+                }
+            }
         }
 
         private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
