c19b12b9bd0422c0d24f661b7757f858d4ceaea2
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-09-30 21:37:29 +0300

    mf

--- DataLayer/DbContextFactory.cs
+++ DataLayer/DbContextFactory.cs
@@ -7,34 +7,34 @@ namespace DataLayer
     public class DbContextFactory
     {
 
-//        private  static string ReleaseConnectionString =
-//            @"User ID = postgres;
-//            Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
-//            Server=127.0.0.1;
-//            Port=5432;
-//            Database=CombatVersion0006;
-//            Integrated Security=true;
-//            Pooling=true;";
-
-        private static string DevelopmentConnectionString =
+        private  static string ReleaseConnectionString =
             @"User ID = postgres;
             Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
-            Server=54.89.247.235;
+            Server=127.0.0.1;
             Port=5432;
-            Database=Dev0165184R;
+            Database=CombatVersion0007;
             Integrated Security=true;
             Pooling=true;";
+
+//        private static string DevelopmentConnectionString =
+//            @"User ID = postgres;
+//            Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
+//            Server=54.89.247.235;
+//            Port=5432;
+//            Database=Dev0165184R;
+//            Integrated Security=true;
+//            Pooling=true;";
 
 
         public static string GetConnectionString()
         {
-            return DevelopmentConnectionString;
+            return ReleaseConnectionString;
         }
  
 
         public  ApplicationContext GetNewDbContext()
         {
-            string connectionString = DevelopmentConnectionString;
+            string connectionString = GetConnectionString();
             
             return new ApplicationContext(
                 new DbContextOptionsBuilder<ApplicationContext>()
