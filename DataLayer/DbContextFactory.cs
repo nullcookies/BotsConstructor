5c109f6d59daf9ca21c7b856807794f0b2dc88b6
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-10-05 19:49:34 +0300

    mf

--- DataLayer/DbContextFactory.cs
+++ DataLayer/DbContextFactory.cs
@@ -5,34 +5,34 @@ namespace DataLayer
     public class DbContextFactory
     {
 
-//        private  static string ReleaseConnectionString =
-//            @"User ID = postgres;
-//            Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
-//            Server=127.0.0.1;
-//            Port=5432;
-//            Database=CombatVersion0006;
-//            Integrated Security=true;
-//            Pooling=true;";
-
-        private static string DevelopmentConnectionString =
+        private  static string ReleaseConnectionString =
             @"User ID = postgres;
             Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
-            Server=54.89.247.235;
+            Server=127.0.0.1;
             Port=5432;
-            Database=Dev_R_04_10_2019_2;
+            Database=CombatVersion0008_guid=02u2h2-f2f2f2-frvebrtc;
             Integrated Security=true;
             Pooling=true;";
+
+//        private static string DevelopmentConnectionString =
+//            @"User ID = postgres;
+//            Password=3t0ssszheM3G4MMM0Ch~n`yparollb_wubfubrkmdbwiyro38;
+//            Server=54.89.247.235;
+//            Port=5432;
+//            Database=Dev_R_04_10_2019_2;
+//            Integrated Security=true;
+//            Pooling=true;";
 
 
         public static string GetConnectionString()
         {
-            return DevelopmentConnectionString;
+            return ReleaseConnectionString;
         }
  
 
         public  ApplicationContext GetNewDbContext()
         {
-            string connectionString = DevelopmentConnectionString;
+            string connectionString = ReleaseConnectionString;
             
             return new ApplicationContext(
                 new DbContextOptionsBuilder<ApplicationContext>()
