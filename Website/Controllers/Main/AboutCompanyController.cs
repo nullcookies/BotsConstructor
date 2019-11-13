59ed6318bb78fd10ad4d45d025c963c930da3eda
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2019-11-13 19:09:31 +0200

    добавил пустой раздел "О кампании"

---
+++ Website/Controllers/Main/AboutCompanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers.Main
+{
+    public class AboutCompanyController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
