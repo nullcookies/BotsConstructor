cd5a3f19c9da45f548ad8be7001908495bebe004
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2019-12-29 16:31:07 +0200

    Улучшение массовой рассылки. Теперь пишет количество ошибок.

--- Website/Services/BotMassMailingService.cs
+++ Website/Services/BotMassMailingService.cs
@@ -16,126 +16,143 @@ namespace Website.Services
 {
     public class BotMassMailingService
     {
-        private readonly ApplicationContext dbContext;
+        private readonly ApplicationContext _dbContext;
 
         public BotMassMailingService(ApplicationContext dbContext)
         {
-            this.dbContext = dbContext;
+            _dbContext = dbContext;
         }
 
-        public async Task MakeMassMailing(int botId, BotMassMailingViewModel model, List<int> stubUSerIds = null)
+        public async Task<int> MakeMassMailing(BotDB botDb, BotMassMailingViewModel model)
         {
-            var botDb = dbContext.Bots.Find(botId);
-            if(botDb == null) throw new Exception("Bot was null.");
-
-            string token = botDb.Token;
-
-            List<int> botUsers = dbContext.BotUsers
+            var botUsers = _dbContext.BotUsers
                 .Where(botUser => botUser.BotUsername == botDb.BotName)
-                .Select(botUser => botUser.BotUserTelegramId)
-                .ToList();
+                .Select(botUser => botUser.BotUserTelegramId);
 
-            if (stubUSerIds != null)
-            {
-                botUsers = stubUSerIds;
-            }
 
-            if (botUsers.Count == 0) return;
+            if (!botUsers.Any()) return 0;
 
-            TelegramBotClient bot = new TelegramBotClient(token);
+            int errorsCount = 0;
+
+            var bot = new TelegramBotClient(botDb.Token);
 
             if (model.File == null)
             {
                 foreach (var userId in botUsers)
                 {
-                    if (model.File == null)
+                    try
                     {
                         await bot.SendTextMessageAsync(userId, model.Text);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        errorsCount++;
                     }
                 }
             }
             else
             {
                 var fileType = model.File.ContentType.Split('/').FirstOrDefault();
-                Func<int, InputOnlineFile, string, Task<Message>> senderFunc;
-                switch (fileType)
-                {
-                    case "image":
-                        senderFunc = (userId, file, text) => bot.SendPhotoAsync(userId, file, text, ParseMode.Markdown);
-                        break;
-                    case "audio":
-                        senderFunc =
-                            ((userId, file, text) => bot.SendAudioAsync(userId, file, text, ParseMode.Markdown));
-                        break;
-                    case "video":
-                        senderFunc = ((userId, file, text) =>
-                            bot.SendVideoAsync(userId, file, caption: text, parseMode: ParseMode.Markdown));
-                        break;
-                    default:
-                        senderFunc = ((userId, file, text) =>
-                            bot.SendDocumentAsync(userId, file, text, ParseMode.Markdown));
-                        break;
-                }
 
                 InputOnlineFile onlineFile;
+                Message msg;
 
-                var firstUserId = botUsers.First();
-                using (var stream = model.File.OpenReadStream())
-                {
-                    onlineFile = new InputOnlineFile(stream);
-                    var msg = await senderFunc(firstUserId, onlineFile, model.Text);
-
-                    switch (msg.Type)
-                    {
-                        case Telegram.Bot.Types.Enums.MessageType.Photo:
-                            onlineFile = new InputOnlineFile(msg.Photo.First().FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendPhotoAsync(userId, file, text, ParseMode.Markdown);
-                            break;
-                        case Telegram.Bot.Types.Enums.MessageType.Audio:
-                            onlineFile = new InputOnlineFile(msg.Audio.FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendAudioAsync(userId, file, text, ParseMode.Markdown);
-                            break;
-                        case Telegram.Bot.Types.Enums.MessageType.Video:
-                            onlineFile = new InputOnlineFile(msg.Video.FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendVideoAsync(userId, file, caption: text, parseMode: ParseMode.Markdown);
-                            break;
-                        case Telegram.Bot.Types.Enums.MessageType.Voice:
-                            onlineFile = new InputOnlineFile(msg.Voice.FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendVoiceAsync(userId, file, text, ParseMode.Markdown);
-                            break;
-                        case Telegram.Bot.Types.Enums.MessageType.Document:
-                            onlineFile = new InputOnlineFile(msg.Document.FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendDocumentAsync(userId, file, text, ParseMode.Markdown);
-                            break;
-                        case Telegram.Bot.Types.Enums.MessageType.Sticker:
-                            onlineFile = new InputOnlineFile(msg.Sticker.FileId);
-                            senderFunc = (userId, file, text) =>
-                                bot.SendStickerAsync(userId, file);
-                            break;
-                        default:
-                            throw new NotImplementedException($"Поддержка сообщений типа {msg.Type} не реализована.");
-                            //break;
-                    }
-                }
-
-                foreach (var userId in botUsers.Skip(1))
+                while (true)
                 {
                     try
                     {
-                        await senderFunc(userId, onlineFile, model.Text);
+                        var firstUserId = botUsers.Skip(errorsCount).First();
+                        using (var stream = model.File.OpenReadStream())
+                        {
+                            onlineFile = new InputOnlineFile(stream);
+                            switch (fileType)
+                            {
+                                case "image":
+                                    msg = await bot.SendPhotoAsync(firstUserId, onlineFile, model.Text,
+                                        ParseMode.Markdown);
+                                    break;
+                                case "audio":
+                                    msg = await bot.SendAudioAsync(firstUserId, onlineFile, model.Text,
+                                        ParseMode.Markdown);
+                                    break;
+                                case "video":
+                                    msg = await bot.SendVideoAsync(firstUserId, onlineFile, caption: model.Text,
+                                        parseMode: ParseMode.Markdown);
+                                    break;
+                                default:
+                                    msg = await bot.SendDocumentAsync(firstUserId, onlineFile, model.Text,
+                                        ParseMode.Markdown);
+                                    break;
+                            }
+                        }
+
+                        break;
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        Console.WriteLine(e);
+                        return errorsCount;
                     }
                     catch (Exception e)
                     {
                         Console.WriteLine(e);
+                        errorsCount++;
+                    }
+                }
+
+                Func<int, Task<Message>> senderFunc;
+                switch (msg.Type)
+                {
+                    case Telegram.Bot.Types.Enums.MessageType.Photo:
+                        onlineFile = new InputOnlineFile(msg.Photo.First().FileId);
+                        senderFunc = (userId) =>
+                            bot.SendPhotoAsync(userId, onlineFile, model.Text, ParseMode.Markdown);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Audio:
+                        onlineFile = new InputOnlineFile(msg.Audio.FileId);
+                        senderFunc = (userId) =>
+                            bot.SendAudioAsync(userId, onlineFile, model.Text, ParseMode.Markdown);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Video:
+                        onlineFile = new InputOnlineFile(msg.Video.FileId);
+                        senderFunc = (userId) =>
+                            bot.SendVideoAsync(userId, onlineFile, caption: model.Text, parseMode: ParseMode.Markdown);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Voice:
+                        onlineFile = new InputOnlineFile(msg.Voice.FileId);
+                        senderFunc = (userId) =>
+                            bot.SendVoiceAsync(userId, onlineFile, model.Text, ParseMode.Markdown);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Document:
+                        onlineFile = new InputOnlineFile(msg.Document.FileId);
+                        senderFunc = (userId) =>
+                            bot.SendDocumentAsync(userId, onlineFile, model.Text, ParseMode.Markdown);
+                        break;
+                    case Telegram.Bot.Types.Enums.MessageType.Sticker:
+                        onlineFile = new InputOnlineFile(msg.Sticker.FileId);
+                        senderFunc = (userId) =>
+                            bot.SendStickerAsync(userId, onlineFile);
+                        break;
+                    default:
+                        throw new NotImplementedException($"Поддержка сообщений типа {msg.Type} не реализована.");
+                }
+
+                foreach (var userId in botUsers.Skip(errorsCount + 1))
+                {
+                    try
+                    {
+                        await senderFunc(userId);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                        errorsCount++;
                     }
                 }
             }
-        }
 
+            return errorsCount;
+        }
     }
 }
