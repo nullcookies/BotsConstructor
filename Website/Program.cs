4bc3ecb97ac452744ae9bfdad991556d04a604ae
Ruslan Starovoitov <starovoytov.ruslan@gmail.com>
2020-04-19 19:52:57 +0300

    оно не хочет запускаться в сервисе

--- Website/Program.cs
+++ Website/Program.cs
@@ -10,12 +10,20 @@ namespace Website
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Намана");
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        private static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            // var host = new WebHostBuilder()
+            //     .UseKestrel()
+            //     .UseContentRoot(Directory.GetCurrentDirectory())
+            //     // .UseIISIntegration()
+            //     .UseStartup<Startup>()
+            return WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("https://localhost:5001/");
+        }
     }
 }
